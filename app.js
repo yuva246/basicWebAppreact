ab00644f2489a160cdf26958c50cb6ca455b2642
yuva246 <65586384+yuva246@users.noreply.github.com>
2020-12-04 12:10:13 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+// Add react app code here
+ class Basic extends React.Component {
+  render() {
+    return(
+    <div className="toolbar"> 
+    <h4> Basic React Web App </h4>
+     </div>
+     
+    );
+  }
+}
+
+ReactDOM.render(<Basic />, document.getElementById('app'))
---
+++ main.js
@@ -0,0 +1,9 @@
+// Add all your JS here
+ function mode() {
+ document.querySelector("body").style.background = "#000";
+ document.querySelector("h5").style.color = "#fff";
+ document.querySelector("h3").style.color = "#fff";
+ document.querySelector("h6").style.color = "#fff";
+ 
+ }
+ 
